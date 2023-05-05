4a18bb89c645627e035cb65c3910c0cb0a266bd3
vinilopes91 <vinilopes13@hotmail.com>
2023-05-04 23:26:52 -0300

    github ci, fix login toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,48 +28,48 @@ const Home: NextPage = () => {
       redirect: false,
     });
 
+    console.log("signIn response", response);
+
     if (response?.status === 401) {
       toast.error("Credenciais inválidas");
-    } else {
+    } else if (!response?.ok) {
       toast.error("Não foi possível realizar o login");
     }
   };
 
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <form
-          className="flex h-screen w-full items-center justify-center"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="card bg-base-100 w-96 shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title">PPGCO</h2>
-              <input
-                type="text"
-                placeholder="Digite seu usuário"
-                className="input input-bordered mt-2 w-full max-w-xs"
-                {...register("username")}
-              />
-              <input
-                type="password"
-                placeholder="Digite sua senha"
-                className="input input-bordered my-2 w-full max-w-xs"
-                {...register("password")}
-              />
-              <div className="card-actions items-center justify-between">
-                <Link href="/cadastro" className="link">
-                  Cadastro
-                </Link>
-                <button className="btn btn-secondary" type="submit">
-                  Login
-                </button>
-              </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <form
+        className="flex h-screen w-full items-center justify-center"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">PPGCO</h2>
+            <input
+              type="text"
+              placeholder="Digite seu usuário"
+              className="input input-bordered mt-2 w-full max-w-xs"
+              {...register("username")}
+            />
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              className="input input-bordered my-2 w-full max-w-xs"
+              {...register("password")}
+            />
+            <div className="card-actions items-center justify-between">
+              <Link href="/cadastro" className="link">
+                Cadastro
+              </Link>
+              <button className="btn btn-secondary" type="submit">
+                Login
+              </button>
             </div>
           </div>
-        </form>
-      </main>
-    </>
+        </div>
+      </form>
+    </main>
   );
 };
 
