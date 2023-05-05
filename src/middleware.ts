ffd0ccc44b3459ae9014a4327505d8e2767c62be
vinilopes91 <vinilopes13@hotmail.com>
2023-05-05 00:23:16 -0300

    corrigido redirect cadastro

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import { Role } from "@prisma/client";
 export default withAuth({
   callbacks: {
     authorized({ req, token }) {
+      console.log("authorized", token);
       if (req.nextUrl.pathname === "/cadastro") {
         return true;
       }
@@ -12,6 +13,7 @@ export default withAuth({
         return token?.role === Role.ADMIN;
       }
       if (req.nextUrl.pathname.startsWith("/candidato")) {
+        console.log("if", token);
         return token?.role === Role.APPLICANT;
       }
       return !!token;
