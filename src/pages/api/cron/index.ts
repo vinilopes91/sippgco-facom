45ae09265c983be7fb3466188f30d5b98cea699c
vinilopes91 <vinilopes13@hotmail.com>
2023-06-10 02:36:57 -0300

    teste cron

--- src/pages/api/cron/index.ts
+++ src/pages/api/cron/index.ts
@@ -2,31 +2,37 @@ import { prisma } from "@/server/db";
 import { NextResponse } from "next/server";
 
 export default async function handler() {
-  const applications = await prisma.application.updateMany({
-    where: {
-      active: true,
-      applicationFilled: false,
-      process: {
-        applicationEndDate: {
-          lt: new Date(),
+  try {
+    const applications = await prisma.application.updateMany({
+      where: {
+        active: true,
+        applicationFilled: false,
+        process: {
+          applicationEndDate: {
+            lt: new Date(),
+          },
         },
       },
-    },
-    data: {
-      applicationFilled: true,
-      status: "REJECTED",
-      reasonForRejection:
-        "Inscrição não foi preenchida dentro do prazo estipulado pelo processo",
-    },
-  });
+      data: {
+        applicationFilled: true,
+        status: "REJECTED",
+        reasonForRejection:
+          "Inscrição não foi preenchida dentro do prazo estipulado pelo processo",
+      },
+    });
 
-  return new NextResponse(
-    JSON.stringify({
-      job: "Indeferir inscrições não preenchidas dentro do prazo",
-      total: applications.count,
-    }),
-    {
-      status: 200,
-    }
-  );
+    return new NextResponse(
+      JSON.stringify({
+        job: "Indeferir inscrições não preenchidas dentro do prazo",
+        total: applications.count,
+      }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return new NextResponse(JSON.stringify(error), {
+      status: 400,
+    });
+  }
 }
