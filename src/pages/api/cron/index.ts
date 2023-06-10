4d732ac36d26a0f72ea5a2a9935e7de3ff123d07
vinilopes91 <vinilopes13@hotmail.com>
2023-06-10 01:56:04 -0300

    cron de indeferir inscricoes fora do prazo

---
+++ src/pages/api/cron/index.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/server/db";
+import { NextResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler() {
+  const applications = await prisma.application.updateMany({
+    where: {
+      active: true,
+      applicationFilled: false,
+      process: {
+        applicationEndDate: {
+          lt: new Date(),
+        },
+      },
+    },
+    data: {
+      applicationFilled: true,
+      status: "REJECTED",
+      reasonForRejection:
+        "Inscrição não foi preenchida dentro do prazo estipulado pelo processo",
+    },
+  });
+
+  return new NextResponse(
+    JSON.stringify({
+      job: "Indeferir inscrições não preenchidas dentro do prazo",
+      total: applications.count,
+    }),
+    {
+      status: 200,
+    }
+  );
+}
